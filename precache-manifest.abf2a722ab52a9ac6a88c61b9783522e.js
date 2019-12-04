c6e686107bf649d5debbd2282931e2930aec3f5a
Dmitro Yagolnik <dima_yagolnik@ukr.net>
2019-12-04 15:37:33 +0200

    Updates

---
+++ precache-manifest.abf2a722ab52a9ac6a88c61b9783522e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ddd7dd49751b7459f97f1c27e61396f",
+    "url": "/goit-react-hw-02-bank-account/index.html"
+  },
+  {
+    "revision": "683dd2afb353b8a5e9e5",
+    "url": "/goit-react-hw-02-bank-account/static/css/main.ed447496.chunk.css"
+  },
+  {
+    "revision": "9bb8fab65c2dfa9c1567",
+    "url": "/goit-react-hw-02-bank-account/static/js/2.91d6526e.chunk.js"
+  },
+  {
+    "revision": "683dd2afb353b8a5e9e5",
+    "url": "/goit-react-hw-02-bank-account/static/js/main.cb5a765e.chunk.js"
+  },
+  {
+    "revision": "a898c5ccd6cf1bcbc60a",
+    "url": "/goit-react-hw-02-bank-account/static/js/runtime-main.1775bb0a.js"
+  }
+]);
